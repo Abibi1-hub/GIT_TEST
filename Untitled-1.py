91ba36d0311972505b55bf8d45fbd7dd3c3a03ef
Abiré <pokoabirepelagie@gmail.com>
2026-01-29 12:20:22 +0100

    Ajout de la fonction addition et d'un test

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,9 +1,17 @@
 print("Bonjour Bonjour, bienvenue dans VS Code !")
+
 # 🟩 1. Affichage simple
 print("Bonjour Abiré, ton environnement Python fonctionne !")
 
-# 🟩 2. Calculs de base
+# 🟩 2. Calculs de base avec une fonction
+def addition(x, y):
+    return x + y
+
 a = 5
 b = 3
-somme = a + b
+somme = addition(a, b)
+
 print(f"Le résultat de {a} + {b} est : {somme}")
+
+# 🟩 3. Nouveau test
+print("Testons une autre addition :", addition(10, 7))
